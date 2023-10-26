9c1601ac2e79729ac82266b2a5543aa59558bf87
Ruboki <ruboki.reuchi@gmail.com>
2023-10-26 17:05:26 +0200

    setting vr body

--- Assets/Scripts/VR/MoveBodyWithCamera.cs
+++ Assets/Scripts/VR/MoveBodyWithCamera.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class MoveBodyWithCamera : MonoBehaviour
 {
-    public GameObject mainCamera;
+    public Transform mainCamera;
     Vector3 positionOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        positionOffset = transform.position - mainCamera.transform.position;
+        positionOffset = transform.position - mainCamera.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = mainCamera.transform.position + positionOffset;
-        transform.rotation = new Quaternion(0, mainCamera.transform.rotation.y, 0, mainCamera.transform.rotation.w);
+        // position
+        transform.position = mainCamera.position + positionOffset;
+
+        // rotation
+        if (transform.rotation.y < mainCamera.rotation.y - 0.25f) transform.rotation = new Quaternion(0, mainCamera.rotation.y - 0.25f, 0, mainCamera.rotation.w);
+        else if (transform.rotation.y > mainCamera.rotation.y + 0.25f) transform.rotation = new Quaternion(0, mainCamera.rotation.y + 0.25f, 0, mainCamera.rotation.w);
     }
 }
