c0c753f0d4d5e2bbd998546985f9ed6f36852230
Ruboki <ruboki.reuchi@gmail.com>
2023-10-26 11:50:04 +0200

    setting vr body

--- Assets/Scripts/VR/Head.cs
+++ Assets/Scripts/VR/Head.cs
@@ -8,7 +8,7 @@ public class Head : MonoBehaviour
     private void LateUpdate()
     {
         rootObject.position = transform.position + headBodyOffset;
-        rootObject.forward = Vector3.ProjectOnPlane(followObject.up, Vector3.up).normalized;
+        rootObject.forward = Vector3.ProjectOnPlane(followObject.forward, Vector3.up).normalized;
 
         transform.position = followObject.TransformPoint(positionOffset);
         transform.rotation = followObject.rotation * Quaternion.Euler(rotationOffset);
