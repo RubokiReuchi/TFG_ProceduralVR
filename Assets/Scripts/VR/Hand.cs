651b42c5e49ca641fa426d04181a56ade60fee8b
Ruboki <ruboki.reuchi@gmail.com>
2023-10-27 10:48:50 +0200

    hands animation and health display

---
+++ Assets/Scripts/VR/Hand.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    [SerializeField] float animationSpeed;
+    Animator animator;
+    float indexTarget;
+    float gripTarget;
+    float indexCurrent;
+    float gripCurrent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    public void SetIndex(float value)
+    {
+        indexTarget = value;
+    }
+
+    public void SetGrip(float value)
+    {
+        gripTarget = value;
+    }
+
+    void AnimateHand()
+    {
+        if (indexCurrent != indexTarget)
+        {
+            indexCurrent = Mathf.MoveTowards(indexCurrent, indexTarget, Time.deltaTime * animationSpeed);
+            animator.SetFloat("Index", indexCurrent);
+        }
+
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * animationSpeed);
+            animator.SetFloat("Grip", gripCurrent);
+        }
+    }
+}
