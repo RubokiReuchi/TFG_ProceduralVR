504723ddb98db8f9110ce93dc035286e435c86ee
Ruboki <ruboki.reuchi@gmail.com>
2023-11-05 09:23:35 +0100

    dash with tunneling

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,8 +15,16 @@ public class PlayerMovement : MonoBehaviour
     Vector3 movement;
 
     [Header("Dash")]
+    [SerializeField] Transform head;
+    [SerializeField] InputActionProperty stickAction;
     [SerializeField] InputActionProperty dashAction;
+    [SerializeField] float dashCooldown;
     float dashCd;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashDuration;
+    [SerializeField] ParticleSystem dashPs;
+    [SerializeField] Material tunnelingMaterial;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +32,8 @@ public class PlayerMovement : MonoBehaviour
         controller = GetComponent<CharacterController>();
         remainingJumps = 0;
         dashCd = 0;
+
+        tunnelingMaterial.SetFloat("_ApertureSize", 1);
     }
 
     // Update is called once per frame
@@ -46,14 +56,41 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(movement * Time.deltaTime);
 
         // dash
-        if (dashCd <= 0 && dashAction.action.ReadValue<float>() == 1)
+        if (dashCd > 0)
         {
-            Debug.Log("Dash");
+            dashCd -= Time.deltaTime;
+            if (dashCd <= 0) dashPs.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+
+        if (IsGrounded() && dashCd <= 0 && dashAction.action.WasPressedThisFrame())
+        {
+            float stickX = stickAction.action.ReadValue<Vector2>().x;
+            float stickY = stickAction.action.ReadValue<Vector2>().y;
+            Quaternion headRotationY = new Quaternion(0, head.rotation.y, 0, head.rotation.w);
+            Vector3 direction = (stickX == 0 && stickY == 0) ? headRotationY * (-transform.forward) : headRotationY * new Vector3(stickX, 0, stickY).normalized;
+            StartCoroutine(DashCo(direction));
         }
     }
 
     bool IsGrounded()
     {
         return Physics.Raycast(transform.position - Vector3.down * 0.1f, Vector3.down, 0.1f);
+    }
+
+    IEnumerator DashCo(Vector3 direction)
+    {
+        dashCd = 999;
+        dashPs.Play();
+        float startTime = Time.time;
+
+        while (Time.time < startTime + dashDuration)
+        {
+            tunnelingMaterial.SetFloat("_ApertureSize", 0.5f);
+
+            controller.Move(dashSpeed * Time.deltaTime * direction);
+            yield return null;
+        }
+        dashCd = dashCooldown;
+        tunnelingMaterial.SetFloat("_ApertureSize", 1);
     }
 }
