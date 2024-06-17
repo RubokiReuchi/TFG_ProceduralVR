a0e7e17a30c3279cdc112de0e044201e30f261df
Ruboki <ruboki.reuchi@gmail.com>
2024-06-17 11:08:26 +0200

    chest sounds

--- Assets/Scripts/Props/Chest.cs
+++ Assets/Scripts/Props/Chest.cs
@@ -13,6 +13,9 @@ public class Chest : MonoBehaviour
     CapsuleCollider leftHandCollider;
     float handInTime;
     Material material;
+    [SerializeField] AudioSource scannerSource;
+    [SerializeField] AudioSource openSource;
+    bool handPlaced = false;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +34,7 @@ public class Chest : MonoBehaviour
     {
         if (colliderBounds.Contains(leftHandCollider.bounds.center))
         {
+            if (handInTime == 0.0f) scannerSource.Play();
             handInTime += Time.deltaTime;
 
             if (handInTime >= 1)
@@ -40,14 +44,36 @@ public class Chest : MonoBehaviour
                 if (type == COIN.BIOMATTER) PlayerSkills.instance.AddBiomatter(amount);
                 else PlayerSkills.instance.AddGear(amount);
                 enabled = false;
+                openSource.Play();
             }
             material.SetColor("_GridColor", new Color(0.35f, 1, 0.54f + 0.46f * handInTime, 1));
+            handPlaced = true;
         }
         else if (handInTime > 0)
         {
+            if (handPlaced)
+            {
+                StartCoroutine(FadeOutScanner());
+                handPlaced = false;
+            }
             handInTime -= Time.deltaTime;
             if (handInTime < 0) handInTime = 0;
             material.SetColor("_GridColor", new Color(0.35f, 1, 0.54f + 0.23f * handInTime, 1));
         }
+    }
+
+    IEnumerator FadeOutScanner()
+    {
+        float initialVolume = scannerSource.volume;
+        float volume = initialVolume;
+        while (volume > 0.0f)
+        {
+            volume -= Time.deltaTime * 2.0f;
+            if (volume < 0.0f) volume = 0.0f;
+            scannerSource.volume = volume;
+            yield return null;
+        }
+        scannerSource.Stop();
+        scannerSource.volume = initialVolume;
     }
 }
